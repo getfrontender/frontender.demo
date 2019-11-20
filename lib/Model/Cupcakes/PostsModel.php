bd01083ee0c7416a1f4426ff410d277e560fde80
Jasper van Rijbroek <jasper@jvar.nl>
2019-11-20 17:10:09 +0100

    Moved both pieces of code to the other models.

--- lib/Model/Cupcakes/PostsModel.php
+++ lib/Model/Cupcakes/PostsModel.php
@@ -5,5 +5,45 @@ declare(strict_types=1);
 namespace Frontender\Platform\Model\Cupcakes;
 
 class PostsModel extends \Frontender\Platform\Model\Wordpress\PostsModel {
+	/**
+	 * This method will return all featured_media from Advanced Custom Fields.
+	 *
+	 * @return array The media items from ACF.
+	 */
+	public function getPropertyFeaturedMedia(): array {
+		if(!isset($this['acf']['featured_media']) && !is_array($this['acf']['featured_media'])) {
+			return [];
+		}
 
+		$mediaIDs = array_column($this['acf']['featured_media'], 'ID');
+		$media = array_map(function($mediaID) {
+			$model = new MediaModel($this->container);
+			$model->setState([
+				'id' => $mediaID
+			]);
+
+			try {
+				$media = $model->fetch();
+				$medium = array_shift($media);
+
+				if(!$medium) {
+					return false;
+				}
+
+				return $medium;
+			} catch(\Error $e) {
+				return false;
+			} catch(\Exception $e) {
+				return false;
+			}
+		}, $mediaIDs);
+
+		return array_filter($media, function($medium) {
+			return !!$medium;
+		});
+	}
+
+	public function getPropertyFeaturedMediaAttribution() {
+		return isset($this['acf']['featured_media_attribution']) && !empty($this['acf']['featured_media_attribution']) ? $this['acf']['featured_media_attribution'] : false;
+	}
 }
